27395edff344182b94dc8706237ca284320e24de
kavibala <kavi03849@gmail.com>
2026-09-18 13:17:04 +0530

    new

---
+++ board.js
@@ -0,0 +1,33 @@
+let countElh = document.getElementById("no-elh")
+console.log (countElh)
+
+let counth = 0
+function oneh() {
+    counth = counth+1
+    countElh.innerText= counth
+}
+function twoh() {
+    counth=counth+2
+    countElh.innerText= counth
+}
+function threeh() {
+    counth=counth+3
+    countElh.innerText= counth
+}
+
+let countElg = document.getElementById("no-elg")
+console.log (countElg)
+
+let countg = 0
+function oneg() {
+    countg=countg+1
+    countElg.innerText= countg
+}
+function twog() {
+    countg=countg+2
+    countElg.innerText= countg
+}
+function threeg() {
+    countg=countg+3
+    countElg.innerText= countg
+}
